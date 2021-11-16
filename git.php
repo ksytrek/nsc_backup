aa492b1dac307bff910be0a9ac1b1b8aa7759136
somphol wila <ksytrek@gmail.com>
2021-11-16 19:55:55 +0700

    .

--- git.php
+++ git.php
@@ -1,6 +1,17 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sync git update</title>
+</head>
+<body>
+    <?php 
 
-$output = shell_exec('git pull');
-echo "<pre>$output</pre>";
+    $output = shell_exec('git pull');
+    echo "<pre>$output</pre>";
 
-?>
+    ?>
+</body>
+</html>
