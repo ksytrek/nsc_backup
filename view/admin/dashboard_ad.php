087c3e6e76da914e0713b69e905082130fcaae86
somphol wila <ksytrek@gmail.com>
2021-11-17 22:12:38 +0700

    a

--- view/admin/dashboard_ad.php
+++ view/admin/dashboard_ad.php
@@ -41,8 +41,20 @@ include_once("./sidebar_ad.php")
 
                 <section id="main-content">
 
+                    
                     <?php
-                    include_once("./report_dasboard_ad.php")
+                    // include_once("./report_dasboard_ad.php");
+
+                    try {
+                        $row = Database::query("SELECT COUNT(*) as total FROM `rooms`;",PDO::FETCH_ASSOC)->fetch();
+
+                        echo"success ".$row['total'];
+    
+                    } catch (Exception $e) {
+                       $error = $e->getMessage();
+                        echo "<script>alert( '{$error}')</script>";
+                    }
+                 
                     ?>
 
                 </section>
--- view/admin/report_dasboard_ad.php
+++ view/admin/report_dasboard_ad.php
@@ -7,6 +7,7 @@
 
 </head>
 
+
 <body>
     <div class="content-wrap">
         <div class="main">
