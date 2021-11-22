799f6b310555949655091121a380290fbbefa1ec
somphol wila <ksytrek@gmail.com>
2021-11-22 11:33:18 +0700

    .

---
+++ raspberrypi_communication/insert.php
@@ -0,0 +1,16 @@
+<?php 
+include_once('../config/connectdb.php');
+
+    $data = [
+        'id_mem' => $value ,
+        'id_room' => $value 
+    ];
+
+    if(Database::insert_data('schedule',$data)){
+        echo "OK!";
+    }
+
+    
+
+
+?>
