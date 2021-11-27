8d0102f94d6d8d4d07231b444ef44f54c38134fa
somphol wila <ksytrek@gmail.com>
2021-11-27 10:26:57 +0700

    4567

--- git.php
+++ git.php
@@ -16,7 +16,7 @@
     // echo "<pre>$output</pre><br/>";
 
 
-    $output = shell_exec('git commit -am "server up to git"');
+    $output = shell_exec('git add -A;git commit -am "server up to git"');
     echo "commit Github ให้ปัจจุบัน";
     echo "<pre>$output</pre><br/>";
 
