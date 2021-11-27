3beef663ce3025a285cc5c25d317c23c51febd4a
somphol wila <ksytrek@gmail.com>
2021-11-27 10:19:21 +0700

    upto git

--- git.php
+++ git.php
@@ -1,22 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sync git update</title>
 </head>
+
 <body>
-    <?php 
+    <?php
+
+    $output = shell_exec('git add -A');
+    echo "Add อมูลจาก server ให้ปัจจุบัน \" git add -A \"";
+    echo "<pre>$output</pre><br/>";
 
     $output = shell_exec('git pull');
     echo "อัพเดตข้อมูลจาก Github ให้ปัจจุบัน";
-    echo "<pre>$output</pre>";
+    echo "<pre>$output</pre><br/>";
 
     $output = shell_exec('git push');
     echo "อัพเดตข้อมูลไปยัง Github ให้ปัจจุบัน";
-    echo "<pre>$output</pre>";
+    echo "<pre>$output</pre><br/>";
+
+    // git add -A
+    // git commit -am "server up to git"
+    // git commit -a
 
     ?>
 </body>
+
 </html>
