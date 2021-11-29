4c58caa616cdac2c4496d750f07d98cafe221536
somphol wila <ksytrek@gmail.com>
2021-11-29 21:51:25 +0700

    os

--- view/personnel/sidebar.php
+++ view/personnel/sidebar.php
@@ -34,7 +34,36 @@ if(isset($_SESSION["key_login"]) && $_SESSION["key_login"] == "personal"){
 </head>
 
 <body>
+<script>
+        function getOS() {
+            var userAgent = window.navigator.userAgent,
+                platform = window.navigator.platform,
+                macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+                windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+                iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+                os = null;
 
+            if (macosPlatforms.indexOf(platform) !== -1) {
+                os = 'Mac OS';
+            } else if (iosPlatforms.indexOf(platform) !== -1) {
+                os = 'iOS';
+            } else if (windowsPlatforms.indexOf(platform) !== -1) {
+                os = 'Windows';
+            } else if (/Android/.test(userAgent)) {
+                os = 'Android';
+            } else if (!os && /Linux/.test(platform)) {
+                os = 'Linux';
+            }
+
+            return os;
+        }
+
+        // alert(getOS());
+
+        const OS_PLATFORM = getOS();
+
+        
+    </script>
     <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
         <div class="nano">
             <div class="nano-content">
