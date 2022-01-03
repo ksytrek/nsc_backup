b85c7c48822bd4bb375458d1efeed55ce88cbce0
SOMPHOL WILA <sompholwilaza101@gmail.com>
2022-01-03 10:09:40 +0700

    เ

--- view/admin/dashboard_ad.php
+++ view/admin/dashboard_ad.php
@@ -8,7 +8,7 @@ include_once("./sidebar_ad.php")
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Focus Admin: Creative Admin Dashboard</title>
+    <title>หน้าหลัก</title>
 
 </head>
 
